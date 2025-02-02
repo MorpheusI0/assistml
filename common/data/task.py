8f3b8d50550db14833c3721d59bb94384ae5ecce
Maximilian Knoll <info@maximilianknoll.de>
2025-02-02 14:45:28 +0100

    ✨ differentiate task types

--- common/data/task.py
+++ common/data/task.py
@@ -1,3 +1,5 @@
+from abc import ABC
+from enum import Enum
 from typing import List, ForwardRef, Optional
 
 from beanie import Document, Link, BackLink
@@ -9,14 +11,20 @@ from .implementation import Implementation
 Model = ForwardRef("Model")
 
 
-class UseCaseSet(BaseModel):
-    task_type: str
-    task_output: str
-
+class TaskType(str, Enum):
+    SUPERVISED_CLASSIFICATION = "Supervised_Classification"
+    SUPERVISED_REGRESSION = "Supervised_Regression"
+    LEARNING_CURVE = "Learning_Curve"
+    SUPERVISED_DATASTREAM_CLASSIFICATION = "Supervised_Datastream_Classification"
+    CLUSTERING = "Clustering"
+    MACHINE_LEARNING_CHALLENGE = "Machine_Learning_Challenge"
+    SURVIVAL_ANALYSIS = "Survival_Analysis"
+    SUBGROUP_DISCOVERY = "Subgroup_Discovery"
+    MULTITASK_REGRESSION = "Multitask_Regression"
 
 class Task(Document):
     mlsea_uri: Optional[str] = None
-    use_case_set: UseCaseSet = Field(alias="UseCaseSet")
+    task_type: TaskType
     dataset: Optional[Link[Dataset]]
     related_implementations: Optional[List[Link[Implementation]]] = Field(default_factory=list)
     #models: Optional[List[BackLink[Model]]] = Field(json_schema_extra={"original_field": "task"})
@@ -25,6 +33,24 @@ class Task(Document):
         name = "tasks"
         keep_nulls = False
         validate_on_save = True
+        use_enum_values = True
+        is_root = True
+
+
+class SupervisedTask(Task, ABC):
+    target_name: str
+
+class ClassificationTask(SupervisedTask):
+    class_labels: Optional[List[str]] = None
+
+class RegressionTask(SupervisedTask):
+    pass
+
+class ClusteringTask(Task):
+    target_name: Optional[str] = None
+
+class LearningCurveTask(ClassificationTask):
+    pass
 
 #from .model import Model
 #Task.model_rebuild()
--- ingestion/processing/task.py
+++ ingestion/processing/task.py
@@ -1,13 +1,17 @@
+import re
 from typing import Optional
-from urllib.parse import urlparse
+
+import openml.tasks
 
 from common.data import Dataset, Task
-from common.data.task import UseCaseSet
+from common.data.task import TaskType, ClassificationTask, RegressionTask, ClusteringTask, LearningCurveTask
 from mlsea import MLSeaRepository
 from mlsea.dtos import TaskDto
 from processing.model import ModelProcessor
 from processing.implementation import ImplementationProcessor
 
+
+MLSO_TT_BASE_URI = "http://w3id.org/mlso/vocab/ml_task_type#"
 
 class TaskProcessor:
     def __init__(self, mlsea: MLSeaRepository):
@@ -40,13 +44,72 @@ class TaskProcessor:
         if task is not None:
             return task
 
-        task = Task(
-            use_case_set=UseCaseSet(
-                task_type=urlparse(task_dto.task_type).fragment,  # TODO: parse task type
-                task_output='single'  # TODO: parse whether it delivers single predictions or class probabilities
-            ),
-            dataset=dataset,
-            mlsea_uri=task_dto.mlsea_task_uri
-        )
+        task = TaskProcessor._parse_task(task_dto, dataset)
+
         await task.insert()
+        return task
+
+    @staticmethod
+    def _parse_task_type(task_type_concept: str) -> TaskType:
+        if not task_type_concept.startswith(MLSO_TT_BASE_URI):
+            raise ValueError(f"Task type concept {task_type_concept} does not start with {MLSO_TT_BASE_URI}")
+
+        task_type_string = task_type_concept[len(MLSO_TT_BASE_URI):]
+
+        # MLSO-TT is not consistently in Capitalized_Snake_Case
+        task_type_string = re.sub(r'(?<!^)(?<!_)([A-Z])', r'_\1', task_type_string)
+
+        # MLSO-TT is not consistent with following OpenML task types
+        if task_type_string == "Learning_Curve_Estimation":
+            task_type_string = "Learning_Curve"
+
+        return TaskType(task_type_string)
+
+    @staticmethod
+    def _parse_task(task_dto, dataset) -> Task:
+        openml_task: openml.tasks.OpenMLTask = openml.tasks.get_task(task_dto.openml_task_id)
+
+        task_type = TaskProcessor._parse_task_type(task_dto.task_type)
+        task: Task
+        if task_type == TaskType.SUPERVISED_CLASSIFICATION:
+            openml_task: openml.tasks.OpenMLClassificationTask
+            task = ClassificationTask(
+                task_type=task_type,
+                dataset=dataset,
+                mlsea_uri=task_dto.mlsea_task_uri,
+                target_name=openml_task.target_name,
+                class_labels=openml_task.class_labels
+            )
+        elif task_type == TaskType.SUPERVISED_REGRESSION:
+            openml_task: openml.tasks.OpenMLRegressionTask
+            task = RegressionTask(
+                task_type=task_type,
+                dataset=dataset,
+                mlsea_uri=task_dto.mlsea_task_uri,
+                target_name=openml_task.target_name
+            )
+        elif task_type == TaskType.CLUSTERING:
+            openml_task: openml.tasks.OpenMLClusteringTask
+            task = ClusteringTask(
+                task_type=task_type,
+                dataset=dataset,
+                mlsea_uri=task_dto.mlsea_task_uri,
+                target_name=openml_task.target_name
+            )
+        elif task_type == TaskType.LEARNING_CURVE:
+            openml_task: openml.tasks.OpenMLLearningCurveTask
+            task = LearningCurveTask(
+                task_type=task_type,
+                dataset=dataset,
+                mlsea_uri=task_dto.mlsea_task_uri,
+                target_name=openml_task.target_name,
+                class_labels=openml_task.class_labels
+            )
+        else:
+            task = Task(
+                task_type=task_type,
+                dataset=dataset,
+                mlsea_uri=task_dto.mlsea_task_uri
+            )
+
         return task
